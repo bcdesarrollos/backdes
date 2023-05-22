497585c5990b5d9b5ee38d50270a3c1d3e6286f7
alemosmusi <alemosmusi@gmail.com>
2023-05-22 12:37:05 +0200

    agregado manual

--- src/db.js
+++ src/db.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const {
-    DB_USER, DB_PASSWORD, DB_HOSTPG, DB_DATABASE, DB_PORT, ACCESS_TOKEN, DB_DEPLOY
-  } = process.env;
-  
-  const sequelize = new Sequelize(`
-  postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOSTPG}:${DB_PORT}/${DB_DATABASE}`, {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  });
-  
+// const sequelize = new Sequelize(`
+// postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOSTPG}:${DB_PORT}/${DB_DATABASE}`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+const sequelize = new Sequelize("postgresql://postgres:WN4LXtI1zdnusJ6BuMkl@containers-us-west-72.railway.app:6268/railway", {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+});
